677a9814a7588157cdaec772343a06ed213e3c61
Marta Fagúndez <55920614+MartaFagundez@users.noreply.github.com>
2025-08-30 01:34:16 +0000

    First test and function done

---
+++ app.js
@@ -0,0 +1,33 @@
+// This is my function that sums two numbers
+const sum = (a,b) => {
+    return a + b
+}
+
+// Just a console log for ourselves
+console.log(sum(7,3))
+
+// one euro is:
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+
+// Declaramos una función con el nombre exacto "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(dollars) {
+    return dollars * oneEuroIs.JPY / oneEuroIs.USD
+}
+
+const fromYenToPound = function(yens) {
+    // to do...
+}
+// Export the function to be used on other files 
+// (similar to the keyword "export" when using webpack)
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
